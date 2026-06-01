fca31e2b0a6a30a4b345405458b8843cddda0858
Wojciech Kluz <wojciech.kluz@microsoft.wsei.edu.pl>
2026-06-01 14:21:00 +0200

    feat(infra): add migration for GradeHistory and PESEL updates

--- src/Infrastructure/EntityFramework/Migrations/20260528162045_AddPeselAndGradeHistory.Designer.cs
+++ src/Infrastructure/EntityFramework/Migrations/20260528162045_AddPeselAndGradeHistory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.EntityFramework.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.EntityFramework.Migrations
 {
     [DbContext(typeof(UniversityDbContext))]
-    partial class UniversityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260528162045_AddPeselAndGradeHistory")]
+    partial class AddPeselAndGradeHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
